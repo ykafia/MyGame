b3795f0aee811cd61bb6090d51d5010013a03132
Youness KAFIA <kafia.youness@gmail.com>
2020-03-17 16:31:56 +0100

    Added ressources and animations

---
+++ MyGame/AnimateBlock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xenko.Core.Mathematics;
+using Xenko.Input;
+using Xenko.Engine;
+using Xenko.Core.Diagnostics;
+
+namespace MyGame
+{
+    public class AnimateBlock : SyncScript
+    {
+        // Declared public member fields and properties will show in the game studio
+
+        public override void Start()
+        {
+            Log.ActivateLog(LogMessageType.Debug);
+        }
+        public override void Update(){
+            var anim = this.Entity.Get<AnimationComponent>();
+            var sk = this.Entity.Get<ModelComponent>().Skeleton;
+            var trf = sk.NodeTransformations[1].Transform.Position;
+            if(anim.IsPlaying("Walk")){
+                Log.Debug("Position of animaton\n" + trf.ToString());
+            }
+            if(Input.IsKeyDown(Keys.O)) {
+                anim.Play("Walk");
+                
+            }
+        }
+    }
+}
---
+++ MyGame/UpdateAfterAnim.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xenko.Core.Mathematics;
+using Xenko.Input;
+using Xenko.Engine;
+using Xenko.Core.Diagnostics;
+
+namespace MyGame
+{
+    public class UpdateAfterAnim : AsyncScript
+    {
+        // Declared public member fields and properties will show in the game studio
+
+        public override async Task Execute()
+        {
+            Log.ActivateLog(LogMessageType.Debug);
+            var anim = this.Entity.Get<AnimationComponent>();
+            var sk = this.Entity.Get<ModelComponent>().Skeleton;
+            var trf = sk.NodeTransformations[0].Transform.Position;
+            while(Game.IsRunning)
+            {
+                // Do stuff everytime the first playing animations has ended
+                
+                if(anim.IsPlaying("Walk")){
+                    var curr = anim.PlayingAnimations.First();
+                    await anim.Ended(curr);
+                    Log.Debug("First\n" + Entity.Transform.Position.ToString());
+                    Log.Debug("Node Position\n"+trf.ToString());
+                }
+                await Script.NextFrame();
+            }
+        }
+    }
+}
